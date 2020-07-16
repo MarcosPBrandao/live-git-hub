9a0ea3eca4e4fb18bc0acf0f9ae841ccfed02e54
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-07-16 13:31:35 -0300

    chore Bonus git Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/teste', (req,res) => {
-  return res.json({ alo: 'mundo 3' })
+  return res.json({ alo: 'mundo 4' })
 } )
 
 app.listen(3333);
